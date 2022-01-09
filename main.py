bafb9bd4eaf9ceb21a55cd0152e878f61a6ea958
qwertyrar1 <tyrsinartem@gmail.com>
2022-01-09 23:11:20 +0500

    Добалена новуая версия стартового экрана. Добавлена возможность выбора персонажа.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from classes import *
 from settings import *
+from random import randint
+
+pygame.display.set_caption('Roguelike Like')
+pygame.font.init()
+
+all_sprites = pygame.sprite.Group()
+enemies_sprites = pygame.sprite.Group()
+walls_sprites = pygame.sprite.Group()
+hero_sprites = pygame.sprite.Group()
 
 choice_hero_id = 0  # 0 - герой не выбран, 1 - эльф, 2 - рыцарь, 3 - ящер, 4 - волшебник
 
@@ -9,11 +18,11 @@ def generate_level(level):
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '.':
-                Tile('empty', x, y, ALL_SPRITES)
+                Tile('empty', x, y, all_sprites)
             elif level[y][x] == '#':
-                Tile('wall', x, y, ALL_SPRITES, WALLS_SPRITES)
+                Tile('wall', x, y, all_sprites, walls_sprites)
             elif level[y][x] == '@':
-                Tile('empty', x, y, ALL_SPRITES)
+                Tile('empty', x, y, all_sprites)
                 # new_player = Player(x, y)
     return new_player, x, y
 
@@ -70,19 +79,15 @@ def start_screen():
         CLOCK.tick(FPS)
 
 
-pygame.display.set_caption('Roguelike Like')
-pygame.font.init()
-
 start_screen()
 
 level_map = load_level('maps\\lvl_1.txt')
 player, max_w, max_h = generate_level(load_level('maps\\lvl_1.txt'))
 
-enemy = Enemy('undeads', 'zombie', ALL_SPRITES, ENEMIES_SPRITES)
+
+enemy = Enemy('undeads', 'big_zombie', all_sprites, enemies_sprites)
 enemy.rect.midbottom = (736, 544)
 enemy.right_faced = False
-enemies_list = [enemy]
-
 
 if choice_hero_id == 1:
     HERO = Hero('elf', 'm', all_sprites, hero_sprites)
@@ -93,11 +98,9 @@ elif choice_hero_id == 3:
 elif choice_hero_id == 4:
     HERO = Hero('wizzard', 'm', all_sprites, hero_sprites)
 
-HERO.rect.center = (0, 0)
-camera = Camera()
-
-
 running = True
+
+frame_counter = 1
 
 while running:
     for event in pygame.event.get():
@@ -119,26 +122,14 @@ while running:
         HERO.right_faced = True
         HERO.anim_type = RUN
         HERO.rect.right += HERO.speed
-    if key[pygame.K_SPACE] or pygame.mouse.get_pressed()[0]:
-        HERO.attack(enemies_list)
     if not ((key[pygame.K_UP] or key[pygame.K_w]) or (key[pygame.K_DOWN] or key[pygame.K_s]) or
-            (key[pygame.K_LEFT] or key[pygame.K_a]) or (key[pygame.K_RIGHT] or key[pygame.K_d]) or
-            ((key[pygame.K_SPACE] or pygame.mouse.get_pressed()[0]) and HERO.attack_is_ready)):
+            (key[pygame.K_LEFT] or key[pygame.K_a]) or (key[pygame.K_RIGHT] or key[pygame.K_d])):
         HERO.anim_type = IDLE
-    if key[pygame.K_ESCAPE]:
-        running = False
 
     SCREEN.fill((0, 0, 0))
 
-    ALL_SPRITES.draw(SCREEN)
-    ALL_SPRITES.update(ENEMIES_SPRITES, HERO, enemies_list)
-    UI_SPRITES.draw(SCREEN)
-    UI_SPRITES.update()
-
-    camera.update(HERO)
-
-    for sprite in ALL_SPRITES:
-        camera.apply(sprite)
+    all_sprites.draw(SCREEN)
+    all_sprites.update(enemies_sprites, HERO)
 
     pygame.display.flip()
     CLOCK.tick(FPS)
